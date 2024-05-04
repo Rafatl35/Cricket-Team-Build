0d27ddcc7469739605fe755127a59e8f28874c1f
Rafatl35 <rafatrahmano4@gmail.com>
2024-05-04 17:48:24 +0600

    .

--- JS/app.js
+++ JS/app.js
@@ -5,21 +5,21 @@ for (const btn of allBtn) {
       const name = event.target.parentNode.parentNode.childNodes[1].innerText;
       const price = event.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
       const category = event.target.parentNode.parentNode.childNodes[5].childNodes[1].innerText;
-      
+
       const selectedContainer = document.getElementById("selected-players-container");
-      const div =document.createElement("div");
-      div.classList.add("flex","text-center","w-full","justify-around")
+      const div = document.createElement("div");
+      div.classList.add("flex", "text-center", "w-full", "justify-around")
 
       const p1 = document.createElement("p");
-      p1.classList.add("text-xl" ,"font-semibold","w-1/3");
+      p1.classList.add("text-xl", "font-semibold", "w-1/3");
       const p2 = document.createElement("p");
-      p2.classList.add("text-xl" ,"font-semibold","w-1/3");
+      p2.classList.add("text-xl", "font-semibold", "w-1/3");
       const p3 = document.createElement("p");
-      p3.classList.add("text-xl" ,"font-semibold","w-1/3");
+      p3.classList.add("text-xl", "font-semibold", "w-1/3");
 
       p1.innerText = name;
       p2.innerText = price;
-      p3.innerText =category;
+      p3.innerText = category;
 
       div.appendChild(p1);
       div.appendChild(p2);
@@ -27,12 +27,24 @@ for (const btn of allBtn) {
 
       selectedContainer.appendChild(div);
 
-      updateTotalCost(price)
+      updateTotalCost(price);
+      updateGrandTotal();
 
    })
 }
 
-function updateTotalCost(value){
+function updateGrandTotal(status) {
+   if (status) {
+      console.log('true');
+   }
+   else {
+      const totalCost = getConvertedValue("total-cost");
+      document.getElementById("grand-total").innerText = totalCost;
+   }
+}
+
+
+function updateTotalCost(value) {
    const totalCost = getConvertedValue("total-cost");
    console.log(totalCost)
    const sum = totalCost + parseInt(value);
