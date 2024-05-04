ff418fcf82ffa1e1872cafb9ab5cf6ca2f0966cd
Rafatl35 <rafatrahmano4@gmail.com>
2024-05-04 08:18:12 +0600

    .

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,48 @@
-function getConvertedValue(id){
+const allBtn = document.getElementsByClassName("add-btn");
+
+for (const btn of allBtn) {
+   btn.addEventListener("click", function (event) {
+      const name = event.target.parentNode.parentNode.childNodes[1].innerText;
+      const price = event.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+      const category = event.target.parentNode.parentNode.childNodes[5].childNodes[1].innerText;
+      
+      const selectedContainer = document.getElementById("selected-players-container");
+      const div =document.createElement("div");
+      div.classList.add("flex","text-center","w-full","justify-around")
+
+      const p1 = document.createElement("p");
+      p1.classList.add("text-xl" ,"font-semibold","w-1/3");
+      const p2 = document.createElement("p");
+      p2.classList.add("text-xl" ,"font-semibold","w-1/3");
+      const p3 = document.createElement("p");
+      p3.classList.add("text-xl" ,"font-semibold","w-1/3");
+
+      p1.innerText = name;
+      p2.innerText = price;
+      p3.innerText =category;
+
+      div.appendChild(p1);
+      div.appendChild(p2);
+      div.appendChild(p3);
+
+      selectedContainer.appendChild(div);
+
+      updateTotalCost(price)
+
+   })
+}
+
+function updateTotalCost(value){
+   const totalCost = getConvertedValue("total-cost");
+   console.log(totalCost)
+   const sum = totalCost + parseInt(value);
+   document.getElementById("total-cost").innerText = sum;
+}
+
+
+
+function getConvertedValue(id) {
    const price = document.getElementById(id).innerText;
-   convertedPrice = parseInt(price);
+   const convertedPrice = parseInt(price);
    return convertedPrice;
 }
